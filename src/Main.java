98475b5000ef9fe11d3df90bd93725b670a67dbb
Samiir Ahmed <samiir.mahamed6@gmail.com>
2023-10-16 17:51:09 +0200

    seconde commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // hej mitt namn är Samiir du då
+        // hej mitt namn är Samiir du dåfefq
 
     }
 }
